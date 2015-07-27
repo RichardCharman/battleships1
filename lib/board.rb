020531099e390a12f9fa919a261869d35d180058
Edwardyao123 <zhigang_edward@hotmail.com>
2015-07-27 12:37:42 +0100

    Initialize Board.class and added a place method for the board

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,20 @@
-class Board
+require_relative 'ship'
+
+class Board 
   require 'terminal-table'
-  
-  def size
-    size = Terminal::Table.new do |t|
-      t << ['A1']
-    
-    end
+
+  def initialize
+  	@Board = []
   end
   
+  def table
+    table = Terminal::Table.new do |t|
+      t << ['A1']
+    end
+  end
+
+  def place(ship)
+  	fail 'Out of bounds' if ship.size > 1
+  	@Board << ship	
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,5 +4,7 @@ describe Board do
   
   it {is_expected.to respond_to :size}
     
-    
-    end
+ #    it 'raise an error if outside of board constraint' do
+ #    expect {subject.size}.to raise_error 'Out of bounds'
+	# end
+end
