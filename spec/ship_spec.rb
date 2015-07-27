11426c6a57bc721a17cce1cb29f117eca54e21f0
Sam Morgan <s_morgan@me.com>
2015-07-27 16:42:16 +0100

    tidied up some specs and heavily refactored board_spec

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,16 +1,10 @@
 require 'ship'
 
 describe Ship do
-
- # it {is_expected.to respond_to(:initialize).with(1).argument}
-
   it 'initializes in a position, with a size and a direction' do
     ship = Ship.new("A1", 3, "N")
     expect(ship.position).to eq "A1" 
     expect(ship.size).to eq 3
     expect(ship.direction).to eq "N"
   end
-  
-#  it {is_expected.to respond_to(:initialize).with(1).argument}
-
 end
