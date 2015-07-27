7065e03c5ae75218e4af20138a58651c51247e35
Richard Charman <richardcharman@live.com>
2015-07-27 11:59:51 +0000

    Added rspec to catch error if ship size larger than table size

--- lib/board.rb
+++ lib/board.rb
@@ -8,13 +8,14 @@ class Board
   end
   
   def table
-    table = Terminal::Table.new do |t|
+    game_board = Terminal::Table.new do |t|
       t << ['A1']
     end
+    print game_board.class
   end
 
   def place(ship)
-  	fail 'Out of bounds' if ship.size > 1
+    fail 'Out of bounds' if ship.size > 1
   	@Board << ship	
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,9 +2,11 @@ require 'board'
 
 describe Board do
   
-  it {is_expected.to respond_to :size}
+  it {is_expected.to respond_to :table}
     
- #    it 'raise an error if outside of board constraint' do
- #    expect {subject.size}.to raise_error 'Out of bounds'
-	# end
+   it 'raise an error if outside of board constraint' do
+     board = Board.new
+     ship = Ship.new("A1",3, "N")
+     expect{subject.place ship}.to raise_error 'Out of bounds'
+	 end
 end
