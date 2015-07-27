32b170aa4a9dcd8ba82ff99fb0ad2729bac1eb48
Edwardyao123 <zhigang_edward@hotmail.com>
2015-07-27 16:02:09 +0100

    fixed overlapping ships

--- lib/board.rb
+++ lib/board.rb
@@ -2,9 +2,11 @@ require_relative 'ship'
 
 class Board 
   require 'terminal-table'
+  attr_accessor :boats
 
+  
   def initialize
-  	@Board = []
+  	 @boats = []
   end
   
   def table
@@ -15,7 +17,20 @@ class Board
   end
 
   def place(ship)
-    fail 'Out of bounds' if ship.size > 1
-  	@Board << ship	
+    if ship.size > 1
+    	fail 'Out of bounds' 
+    elsif overlap(ship) == true
+    	fail 'occupied' 
+    else
+    	boats << ship	
+    end
+  end
+
+  def overlap(ship)
+  	if @boats.map{|ship| ship.position}.include?(ship.position)
+  		return true
+  	else 
+  		return false
+  	end
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,9 +4,18 @@ describe Board do
   
   it {is_expected.to respond_to :table}
     
-   it 'raise an error if outside of board constraint' do
+  it 'raise an error if outside of board constraint' do
      board = Board.new
      ship = Ship.new("A1",3, "N")
      expect{subject.place ship}.to raise_error 'Out of bounds'
-	 end
+  end
+
+  it 'raise an error if ship overlap' do
+    board = Board.new
+    ship1 = Ship.new("A1",1,"N")
+    board.place ship1
+    ship2 = Ship.new("A1",1,"N")
+    expect{board.place ship2}.to raise_error 'occupied'
+  end
+
 end
